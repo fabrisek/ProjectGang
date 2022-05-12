1918885cd37ccd897a9d948171dd8facdd9800d4
JeremyFerreira <91945288+JeremyFerreira@users.noreply.github.com>
2022-05-12 10:30:07 +0200

    fps avec new InputSystem (marche bizzarement)

---
+++ Assets/Scripts/Fps/PlayerCam.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.InputSystem;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX = 1;
+    public float sensY = 1;
+
+    public Transform orientation;
+    public Transform camHolder;
+
+    float xRotation;
+    float yRotation;
+    private Vector2 mousePosition;
+
+     Input inputActions;
+
+    private void Awake()
+    {
+        inputActions = new Input();
+    }
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        DoLooking();
+
+        
+    }
+    private void DoLooking()
+    {
+        //getMouse Inputs
+        Vector2 looking = GetPlayerLook();
+        float lookX = looking.x * sensX * Time.deltaTime;
+        float lookY = looking.y * sensY * Time.deltaTime;
+
+        yRotation -= lookX;
+        xRotation -= lookY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        // rotate cam and orientation
+        camHolder.rotation = Quaternion.Euler(xRotation, -yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, -yRotation, 0);
+    }
+    public Vector2 GetPlayerLook()
+    {
+        return inputActions.InGame.Look.ReadValue<Vector2>();
+    }
+
+    public void DoFov(float endValue)
+    {
+        GetComponent<Camera>().DOFieldOfView(endValue, 0.25f);
+    }
+
+    public void DoTilt(float zTilt)
+    {
+        transform.DOLocalRotate(new Vector3(0, 0, zTilt), 0.25f);
+    }
+}
