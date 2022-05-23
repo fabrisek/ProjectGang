2a89f2f6d580eb5efa35cfd119d6fdd6113f5dbd
JeremyFerreira <91945288+JeremyFerreira@users.noreply.github.com>
2022-05-23 20:53:23 +0200

    audioSpectrum

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioClip[] soundEffects, music;
+    [SerializeField] [Range(0, 100)] float soundEffectsVolume, musicVolume;
+    [SerializeField] AudioSource audioSourceSoundEffect, audioSourceMusic;
+
+    // Start is called before the first frame update
+    
+    private void Awake()
+    {
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeVolumeMusic(musicVolume/100);
+        ChangeVolumeSoundEFFect(soundEffectsVolume/100);
+    }
+
+    public void ChangeVolumeSoundEFFect(float volume)
+    {
+        audioSourceSoundEffect.volume = volume;
+    }
+    public void ChangeVolumeMusic(float volume)
+    {
+        audioSourceMusic.volume = volume;
+    }
+    public void PlayMusic(int index)
+    {
+        audioSourceMusic.clip = music[index];
+        audioSourceMusic.Play();
+    }
+    public void playSoundEffect(int index)
+    {
+        audioSourceSoundEffect.clip = soundEffects[index];
+        audioSourceSoundEffect.Play();
+
+    }
+    
+}
