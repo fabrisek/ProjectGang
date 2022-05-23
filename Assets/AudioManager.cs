dc6a17578e0339c454261a60084d920ce60c5e87
JeremyFerreira <91945288+JeremyFerreira@users.noreply.github.com>
2022-05-23 21:06:14 +0200

    audio

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -12,7 +12,7 @@ public class AudioManager : MonoBehaviour
     
     private void Awake()
     {
-        DontDestroyOnLoad(this);
+        DontDestroyOnLoad(transform.gameObject);
     }
     void Start()
     {
