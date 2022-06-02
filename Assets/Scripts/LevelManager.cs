09731661ac0b81b65ab71e08848c6281a82a971c
Fabrice C <47283393+fabrisek@users.noreply.github.com>
2022-06-02 23:06:05 +0200

    Push

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -81,7 +81,10 @@ public class LevelManager : MonoBehaviour
     {
 
             if (HudControllerInGame.Instance.InMenu == false)
+        {
+            Time.timeScale = 1;
                 LevelLoader.Instance.LoadLevel(SceneManager.GetActiveScene().buildIndex);
+        }
 
             else
             {
