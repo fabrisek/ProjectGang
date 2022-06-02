e46f1c093849ab10b7b8328b5d335ed54731c267
KaprisDimitri <kaprisdimitri@gmail.com>
2022-06-02 21:34:17 +0200

    AjouttstCineRglageLoad

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -41,9 +41,11 @@ public class LevelLoader : MonoBehaviour
             float progress = Mathf.Clamp01(operation.progress / .9f);
             _loadingSlider.value = progress;
             _textSlider.text = progress * 100f + "%";
-            yield return new WaitForSeconds(2);
-            _loadingScreenPanel.SetActive(false);
-            Time.timeScale = 1;
+           
         }
+
+        yield return new WaitForSeconds(2);
+        _loadingScreenPanel.SetActive(false);
+        Time.timeScale = 1;
     }
 }
