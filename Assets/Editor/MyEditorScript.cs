cacb04e13d7955a9118dc6661c2331d8a6466a43
Fabrice C <47283393+fabrisek@users.noreply.github.com>
2022-09-12 14:42:44 +0200

    Update MyEditorScript.cs

--- Assets/Editor/MyEditorScript.cs
+++ Assets/Editor/MyEditorScript.cs
@@ -32,7 +32,7 @@ class MyEditorScript {
 
         static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
         {
-                EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+                SwitchActiveBuildTarget(build_target);
                 string res = BuildPipeline.BuildPlayer(scenes,target_dir,build_target,build_options);
                 if (res.Length > 0) {
                         throw new Exception("BuildPlayer failure: " + res);
